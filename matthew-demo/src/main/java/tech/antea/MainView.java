70e6b132d5d5857935c3d91b609d7880290be41b
Antea <antea-matthew@matthews-mbp-6.home>
2023-04-27 16:18:42 -0400

    display basic map

--- matthew-demo/src/main/java/tech/antea/MainView.java
+++ matthew-demo/src/main/java/tech/antea/MainView.java
@@ -4,16 +4,28 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import software.xdev.vaadin.maps.leaflet.flow.LMap;
+import software.xdev.vaadin.maps.leaflet.flow.data.LTileLayer;
 
 /**
  * The main view contains a button and a click listener.
  */
 @Route
 public class MainView extends VerticalLayout {
+    private LMap map;
 
     public MainView() {
         Button button = new Button("Click me",
                 event -> Notification.show("Clicked!"));
-        add(button);
+
+        this.map = new LMap(49.675126, 12.160733, 17);
+        this.map.setTileLayer(LTileLayer.DEFAULT_OPENSTREETMAP_TILE);
+        this.map.setSizeFull();
+        add(this.map, button);
+        this.setSizeFull();
     }
+
+
+
+
 }
