a13420d127c44b0fd16c67586779b13c9a231a8c
Antea <antea-matthew@matthews-mbp-6.home>
2023-04-27 16:40:04 -0400

    add antea office marker to map

--- matthew-demo/src/main/java/tech/antea/MainView.java
+++ matthew-demo/src/main/java/tech/antea/MainView.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import software.xdev.vaadin.maps.leaflet.flow.LMap;
+import software.xdev.vaadin.maps.leaflet.flow.data.LMarker;
 import software.xdev.vaadin.maps.leaflet.flow.data.LTileLayer;
 
 /**
@@ -17,9 +18,13 @@ public class MainView extends VerticalLayout {
     public MainView() {
         Button button = new Button("Click me",
                 event -> Notification.show("Clicked!"));
-
-        this.map = new LMap(49.675126, 12.160733, 17);
+        // coordinates of Montreal Antea office
+        // 45.45235633938714, -73.46762601648902
+        this.map = new LMap(45.45235633938714, -73.4676260164890, 17);
+        final LMarker anteaMarker = new LMarker(45.45235633938714, -73.46762601648902);
+        anteaMarker.setPopup("Antea Montreal Office");
         this.map.setTileLayer(LTileLayer.DEFAULT_OPENSTREETMAP_TILE);
+        this.map.addLComponents(anteaMarker);
         this.map.setSizeFull();
         add(this.map, button);
         this.setSizeFull();
