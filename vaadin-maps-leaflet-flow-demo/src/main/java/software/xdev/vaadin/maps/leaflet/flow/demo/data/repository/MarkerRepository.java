88ccc40b2a79599436bf9da5d7c55f56d2b3fdc4
Matthew Toledo <toledo.matthew@anteash.com>
2023-05-08 12:18:02 -0400

    fix pom.xml depdency

--- vaadin-maps-leaflet-flow-demo/src/main/java/software/xdev/vaadin/maps/leaflet/flow/demo/data/repository/MarkerRepository.java
+++ vaadin-maps-leaflet-flow-demo/src/main/java/software/xdev/vaadin/maps/leaflet/flow/demo/data/repository/MarkerRepository.java
@@ -2,6 +2,7 @@ package software.xdev.vaadin.maps.leaflet.flow.demo.data.repository;
 
 import software.xdev.vaadin.maps.leaflet.flow.demo.data.entity.Marker;
 
+
 public interface MarkerRepository {
 
 }
--- vaadin-maps-leaflet-flow-demo/src/main/java/software/xdev/vaadin/maps/leaflet/flow/demo/data/service/DbService.java
+++ vaadin-maps-leaflet-flow-demo/src/main/java/software/xdev/vaadin/maps/leaflet/flow/demo/data/service/DbService.java
@@ -1,7 +1,7 @@
 package software.xdev.vaadin.maps.leaflet.flow.demo.data.service;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Service
 public class DbService {
