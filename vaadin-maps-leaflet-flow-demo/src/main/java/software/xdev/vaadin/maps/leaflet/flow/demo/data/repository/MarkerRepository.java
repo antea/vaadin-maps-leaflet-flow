d8d2d2a8f137ab58434f4aa8c7f529f7a64fc8ec
Matthew Toledo <toledo.matthew@anteash.com>
2023-05-08 12:12:04 -0400

    add dependencies to pom.xml

--- vaadin-maps-leaflet-flow-demo/src/main/java/software/xdev/vaadin/maps/leaflet/flow/demo/data/repository/MarkerRepository.java
+++ vaadin-maps-leaflet-flow-demo/src/main/java/software/xdev/vaadin/maps/leaflet/flow/demo/data/repository/MarkerRepository.java
@@ -1,8 +1,7 @@
 package software.xdev.vaadin.maps.leaflet.flow.demo.data.repository;
 
 import software.xdev.vaadin.maps.leaflet.flow.demo.data.entity.Marker;
-import org.springframework.jpa.repository
 
-public interface MarkerRepository extends JpaRepository<Marker, Long> {
+public interface MarkerRepository {
 
 }
