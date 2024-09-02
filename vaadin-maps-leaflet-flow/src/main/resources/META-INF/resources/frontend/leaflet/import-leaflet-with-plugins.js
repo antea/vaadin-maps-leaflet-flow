42d82b3460b24c6646fdaae47bb73f607f7fc838
Cristina Attori <cristina.attori@antea.tech>
2024-09-02 17:27:52 +0200

    Closes VML-4 MarkerClusterGroup plugin

---
+++ vaadin-maps-leaflet-flow/src/main/resources/META-INF/resources/frontend/leaflet/import-leaflet-with-plugins.js
@@ -0,0 +1,4 @@
+// We cannot import Leaftlet and the plugins using many @JsModule annotations, because Vaadin has a bug that does not
+// guarantee that the imports will be in the same order as defined with @JsModule: https://github.com/vaadin/flow/issues/15825
+import 'leaflet/dist/leaflet.js';
+import 'leaflet.markercluster/dist/leaflet.markercluster.js';
